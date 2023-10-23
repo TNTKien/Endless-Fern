a425741ed815a26357d59146eab211b084bcedc4
iamneyk <iamneyk@gmail.com>
2023-10-23 22:12:01 +0700

    upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var maxHeightScrolled = 0;
+window.onscroll = function() {
+    var heightScrolled = window.scrollY;
+
+    // if (heightScrolled > maxHeightScrolled) {
+    //     maxHeightScrolled = heightScrolled;
+    // }
+
+    //var fernHeight = maxHeightScrolled*0.05645 + 165;
+    var fernHeight = heightScrolled*0.05645 + 165;
+
+    var heightScrolledElement = document.getElementById("header");
+    heightScrolledElement.innerHTML = "Your Fern now is " + fernHeight + "cm";
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        var imageContainer = document.getElementById("image-container");
+        imageContainer.innerHTML += `
+            <img src="imgs/image4.jpg">
+            <img src="imgs/image5.jpg">
+            <img src="imgs/image6.jpg">
+            <img src="imgs/image7.jpg">
+            <img src="imgs/image8.jpg">
+            <img src="imgs/image9.jpg">
+        `;
+    }
+};
+
+var backToTopButton = document.getElementById("back-to-top");
+backToTopButton.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
